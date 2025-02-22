6b7df25677bc15f2d9469e881b8a45782caa1e61
Hitoshi <hitoshiwtb1104@gmail.com>
2025-02-22 22:09:49 +0900

    Initial Commit

---
+++ Igo/IgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IgoApp.swift
+//  Igo
+//
+//  Created by hitoshi on 2025/02/22.
+//
+
+import SwiftUI
+
+@main
+struct IgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
